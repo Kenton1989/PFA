eb2bd4f6a70c06f3ded76bebf11ee329a32d3e69
kenton <38125586+Kenton1989@users.noreply.github.com>
2020-10-08 22:37:19 +0800

    Refine class UserProfile
    Add enum AcademicQualification
    Refine class UserProfile
    - Add age attribute
    - Add academic qualification attribute
    Add possible type comversion code for enum Gender

---
+++ app/src/main/java/sg/edu/ntu/gg4u/pfa/persistence/AcademicQualification.java
@@ -0,0 +1,6 @@
+package sg.edu.ntu.gg4u.pfa.persistence;
+
+public enum AcademicQualification {
+    // TODO: Fill the qualification, add conversion support
+    UNKNOWN;
+}
--- app/src/main/java/sg/edu/ntu/gg4u/pfa/persistence/Gender.java
+++ app/src/main/java/sg/edu/ntu/gg4u/pfa/persistence/Gender.java
@@ -1,5 +1,39 @@
 package sg.edu.ntu.gg4u.pfa.persistence;
 
 enum Gender {
-    UNKNOWN, MALE, FEMALE, SPECIAL
+    // TODO: add conversion support
+    UNKNOWN, MALE, FEMALE, SPECIAL;
+
+//    public int toInt() {
+//        switch (this) {
+//            case UNKNOWN:
+//                return 0;
+//            case MALE:
+//                return 1;
+//            case FEMALE:
+//                return 2;
+//            case SPECIAL:
+//                return 3;
+//            default:
+//                return -1;
+//        }
+//    }
+//
+//    public static int toInt(Gender g) {
+//        return g.toInt();
+//    }
+//
+//    public static Gender fromInt(int val) {
+//        switch (val) {
+//            case 1:
+//                return MALE;
+//            case 2:
+//                return FEMALE;
+//            case 3:
+//                return SPECIAL;
+//            default:
+//                return UNKNOWN;
+//        }
+//    }
+
 }
