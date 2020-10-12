d8eaaa6a5570e81c86f29a1e5fd08e6cad9fdd2c
wzy27 <wzy27@users.noreply.github.com>
2020-10-12 13:36:52 +0800

    update database code
    add UserProfile Entity

--- app/src/main/java/sg/edu/ntu/gg4u/pfa/persistence/Category.java
+++ app/src/main/java/sg/edu/ntu/gg4u/pfa/persistence/Category.java
@@ -11,26 +11,16 @@ import java.util.UUID;
 @Entity(tableName = "Category")
     public class Category {
 
-    private static int maxId = 10; // change to dynamic calculation after...
-
     @PrimaryKey
-    private int categoryId;
-
     private String name;
 
     @Ignore
     public Category(String _name) {
-        categoryId = ++maxId;
         name = _name;
     }
 
     public Category() {
-        categoryId = Integer.MAX_VALUE;
         name = "dummy";
-    }
-
-    public int getCategoryId() {
-        return categoryId;
     }
 
     public String getName() {
--- app/src/main/java/sg/edu/ntu/gg4u/pfa/persistence/CategoryDao.java
+++ app/src/main/java/sg/edu/ntu/gg4u/pfa/persistence/CategoryDao.java
@@ -1,8 +1,15 @@
 package sg.edu.ntu.gg4u.pfa.persistence;
 
 import androidx.room.Dao;
+import androidx.room.Query;
+
+import io.reactivex.Flowable;
 
 @Dao
 public interface CategoryDao {
+    @Query("SELECT * FROM Category")
+    Flowable<Category> getAllCategory();
 
+    @Query("DELETE FROM Category")
+    void deleteCategory(String name);
 }
