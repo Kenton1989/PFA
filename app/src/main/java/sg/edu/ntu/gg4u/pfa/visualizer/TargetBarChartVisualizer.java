70dd24128433edbaf8f67b5de54f2836f2d6dc54
kenton <38125586+Kenton1989@users.noreply.github.com>
2020-10-15 14:27:53 +0800

    Bug Fix

---
+++ app/src/main/java/sg/edu/ntu/gg4u/pfa/visualizer/TargetBarChartVisualizer.java
@@ -0,0 +1,9 @@
+package sg.edu.ntu.gg4u.pfa.visualizer;
+
+import com.androidplot.xy.XYPlot;
+
+public class TargetBarChartVisualizer {
+    public void plot(XYPlot xyPlot, String[] category, Number[] target, Number[] cost) {
+
+    }
+}
