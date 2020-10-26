5d0a2bdecfa9cae849d42a7dd6339a46177c5f1b
ykaiw <63635397+ykaiw@users.noreply.github.com>
2020-10-26 15:06:06 +0800

    create category popup

--- app/src/main/java/sg/edu/ntu/gg4u/pfa/ui/category/CustomListCategory.java
+++ app/src/main/java/sg/edu/ntu/gg4u/pfa/ui/category/CustomListCategory.java
@@ -5,17 +5,23 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.TextView;
+
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.FragmentActivity;
+
+import java.util.ArrayList;
 
 import sg.edu.ntu.gg4u.pfa.R;
 
 public class CustomListCategory extends ArrayAdapter<String>{
 
     private final Activity context;
-    private final String[] catList;
+    private final ArrayList<String> catList;
 
     public CustomListCategory(Activity context,
-                              String[] catList) {
+                              ArrayList<String> catList) {
         super(context, R.layout.activity_category_listview, catList);
         this.context = context;
         this.catList = catList;
@@ -23,11 +29,19 @@ public class CustomListCategory extends ArrayAdapter<String>{
 
     }
     @Override
-   public View getView(int position, View view, ViewGroup parent) {
+   public View getView(int position, final View view, final ViewGroup parent) {
         LayoutInflater inflater = context.getLayoutInflater();
         View rowView= inflater.inflate(R.layout.activity_category_listview, null, true);
+        TextView delete = (TextView) rowView.findViewById(R.id.deleteCat);
+//                .setOnClickListener(new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View v) {
+//                        DialogFragment createFrag = new CreateCategoryFragment();
+//                        createFrag.show(getSupportFragmentManager(), "create");
+//                    }
+//                });
         TextView catTitle = (TextView) rowView.findViewById(R.id.catList);
-        catTitle.setText(catList[position]);
+        catTitle.setText(catList.get(position));
 
         return rowView;
     }
