5c589277b682e425321e4f095deb9cb1cc16097e
ykaiw <63635397+ykaiw@users.noreply.github.com>
2020-11-01 15:19:42 +0800

    Revert "Revert "Merge branch 'database' into kai-viz""
    This reverts commit ea2bbd1f203bcc48860effb1e15fbeb3fa779fe7.

--- app/src/main/java/sg/edu/ntu/gg4u/pfa/ui/category/CategoryViewModel.java
+++ app/src/main/java/sg/edu/ntu/gg4u/pfa/ui/category/CategoryViewModel.java
@@ -1,31 +1,43 @@
 package sg.edu.ntu.gg4u.pfa.ui.category;
 
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
 import androidx.lifecycle.ViewModel;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import io.reactivex.Completable;
 import io.reactivex.Flowable;
 import sg.edu.ntu.gg4u.pfa.persistence.Category.Category;
+import sg.edu.ntu.gg4u.pfa.persistence.Target.Target;
 import sg.edu.ntu.gg4u.pfa.ui.CategoryDataSource;
+import sg.edu.ntu.gg4u.pfa.ui.TargetDataSource;
 
 public class CategoryViewModel extends ViewModel {
 
-    private final CategoryDataSource mDataSource;
+    private final CategoryDataSource mCategoryDataSource;
+    private final TargetDataSource mTargetDataSource;
 
-    public CategoryViewModel(CategoryDataSource categoryDataSource) {
-        mDataSource = categoryDataSource;
+    public CategoryViewModel(CategoryDataSource categoryDataSource,
+                             TargetDataSource targetDataSource) {
+        mCategoryDataSource = categoryDataSource;
+        mTargetDataSource = targetDataSource;
     }
 
     public Flowable<List<Category>> getAllCategory() {
-        return mDataSource.getAllCategory();
+        return mCategoryDataSource.getAllCategory();
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     public Completable createNewCategory(Category category) {
-        return mDataSource.createNewCategory(category);
+        return mCategoryDataSource.createNewCategory(category)
+                .concatWith(mTargetDataSource.insertOrUpdateTarget(
+                        new Target(category.getName(), 0)));
     }
 
     public Completable deleteCategory(Category category) {
-        return mDataSource.deleteCategory(category);
+        return mCategoryDataSource.deleteCategory(category);
     }
 }
